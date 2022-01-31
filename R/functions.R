c7f3f412eeb2880237508e4b03b42acd19ff3a3c
Joel Nitta <joelnitta@gmail.com>
2022-01-31 18:08:10 +0900

    Update MS
    Add phylogeny figures

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,17 @@
+# Formatting ----
+
+# Specify percentage format
+percent <- function(...) {scales::percent(...)}
+
+# Specify number format
+number <- function(...) {scales::number(big.mark = ",", ...)}
+
+# Abbreviations
+ie <- "*i.e.*"
+eg <- "*e.g.*"
+
+# References ----
+
 #' Extract citations (formatted like `[@key]`) from an Rmd file
 #'
 #' @param rmd_file Character vector; path to Rmd file
@@ -61,3 +75,63 @@ filter_refs_yaml <- function(rmd_file, yaml_in = "ms/main_library.yaml", yaml_ou
 	# Write out the YAML file
 	yaml::write_yaml(ref_yaml_filtered, file = yaml_out)
 }
+
+# Figure output ----
+
+#' Generate a path to save a results file
+#' 
+#' Only works for figures or tables cited in text, and outputs
+#' files to the "results" folder
+#'
+#' @param result_num Number of result, e.g. "Fig. 2"
+#' @param extension Extension to use for file
+#'
+#' @return String.
+#' 
+result_file <- function (result_num, extension) {
+	
+	fs::path(
+		here::here("results"),
+		result_num %>%
+			str_remove_all("\\.") %>% 
+			str_replace_all(" ", "_")
+	) %>%
+		fs::path_ext_set(extension)
+}
+
+# Captions ----
+
+# Follow Frontiers in Plant Sciences style
+# Captions should be preceded by the appropriate label,
+# for example "Figure 1." 
+# Figure panels are referred to by bold capital letters in brackets: 
+# (A), (B), (C), (D), etc.
+
+# - First define the "full" version, which would include a caption
+# (except I never use the caption in the function, and instead replace with 'blank')
+figure_full <- captioner::captioner(prefix = "Figure ", auto_space = FALSE)
+table_full <- captioner::captioner(prefix = "Table ", auto_space = FALSE)
+s_figure_full <- captioner::captioner(prefix = "Figure S ", auto_space = FALSE)
+s_table_full <- captioner::captioner(prefix = "Table S ", auto_space = FALSE)
+
+# - Make a short function that prints only the object type and number, e.g., "Fig. 1"
+figure <- pryr::partial(figure_full, display = "cite", caption = "blank")
+table <- pryr::partial(table_full, display = "cite", caption = "blank")
+s_figure <- pryr::partial(s_figure_full, display = "cite", caption = "blank")
+s_table <- pryr::partial(s_table_full, display = "cite", caption = "blank")
+
+# - Make a short function that prints only the number (e.g., "1")
+figure_num <- function(name) {figure(name) %>% str_remove("Figure ")}
+table_num <- function(name) {table(name) %>% str_remove("Table ")}
+s_figure_num <- function(name) {s_figure(name) %>% str_remove("Figure S ")}
+s_table_num <- function(name) {s_table(name) %>% str_remove("Table S ")}
+
+# Pagebreaks ----
+
+#' Pagebreak for PDF only
+#' 
+#' Meant for use within Rmd when rendering. Requires parameter variable
+#' `doc_type` to be defined (either 'doc' for MS Word or 'pdf' for PDF output)
+#'
+#' Breaks a page depending if the output format is PDF
+pagebreak_pdf <- function(rmd_params = params) {ifelse(rmd_params$doc_type == "pdf", return("\\clearpage"), return(""))}
