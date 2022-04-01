bb850335ce2e422c38679239fbd88281e6d0daf3
Joel Nitta <joelnitta@gmail.com>
2022-04-01 12:31:17 +0900

    Update MS

--- R/process_refs.R
+++ R/process_refs.R
@@ -3,11 +3,11 @@ library(tidyverse)
 library(rmdref)
 
 # Filter YAML references from Zotero library.
-# data_raw/main_library.yaml has been exported from Zotero
+# main_library.yaml has been exported from Zotero
 # like this: file -> "export library" -> "Better CSL YAML"
 filter_refs_yaml(
   rmd_file = "ms/manuscript.Rmd",
-  yaml_in = "data_raw/main_library.yaml",
+  yaml_in = "_targets/user/data_raw/main_library.yaml",
   yaml_out = "ms/references.yaml", silent = TRUE)
 
 # Same for SI
@@ -16,7 +16,7 @@ filter_refs_yaml(
     "ms/si.rmd", # SI
     "_targets/user/data_raw/pteridocat_ppgi_diff_notes.csv" # refs in SI tables
   ),
-  yaml_in = "data_raw/main_library.yaml",
+  yaml_in = "_targets/user/data_raw/main_library.yaml",
   yaml_out = "ms/si_references.yaml", silent = TRUE)
 
 # Write out 'aux' file with citation keys to make collection in Zotero
